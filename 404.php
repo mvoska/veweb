57f4abd46bc31644ed0788da5c9c0720bd4c216f
Matt Voska <mattvoska@gmail.com>
2013-06-27 01:13:01 -0400

    converted contact and 404 page to php, added external php content, edited links

---
+++ 404.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> 
+<html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+  <title>Page Under Development</title>
+  <?php include("head.php"); ?>
+</head>
+<body class="collapsing_header">
+  <header>
+    <div class="container">
+      <div class="navbar">
+        <div class="navbar-inner"> 
+          <?php include("logo.php"); ?>
+          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"><span class="nb_left pull-left"> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span></span> <span class="nb_right pull-right">menu</span> </a>
+          <div class="nav-collapse collapse">
+            <ul class="nav pull-right">
+              <li><a href="index.php">Home</a></li>
+              <li><a href="#">About Us</a></li>
+              <li><a href="#">Services</a></li>
+              <li><a href="#">Portfolio</a></li>
+              <li><a href="#">Blog</a></li>
+              <li><a href="contact.php">Contact</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <?php include("social_media_top.php"); ?>
+    </div>
+  </header>
+  <div class="full_page_photo" style="background-image: url(images/404.jpg);">
+    <div class="container">
+      <section class="call_to_action">
+        <h3 class="animated bounceInDown">OMG! <strong>error 404</strong></h3>
+        <h4 class="animated bounceInUp">we are really sorry but the page you requested cannot be found.</h4>
+      </section>
+    </div>
+  </div>
+  <div id="main">
+    <div class="container">
+      <div class="hgroup">
+        <h1>Page not found</h1>
+        <ul class="breadcrumb pull-right">
+          <li class="active">Something went wrong!</li>
+        </ul>
+      </div>
+      <section class="call_to_action"> <i class="icon-ambulance icon-4x"></i>
+        <h3>error 404 is nothing to really worry about...</h3>
+        <h4>you may have mis-typed the URL, please check your spelling and try again.</h4>
+      </section>
+    </div>
+    <?php include("footer.php"); ?>
+  </div>
+<?php include("scripts.php"); ?>
+</body>
+</html>
---
+++ contact.php
@@ -0,0 +1,94 @@
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> 
+<html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+ <title>Voska Electronics | Contact</title>
+ <?php include("head.php"); ?>
+</head>
+<body class="collapsing_header">
+  <header>
+    <div class="container">
+      <div class="navbar">
+        <div class="navbar-inner"> 
+          <?php include("logo.php"); ?>
+          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"><span class="nb_left pull-left"> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span></span> <span class="nb_right pull-right">menu</span> </a>
+          <div class="nav-collapse collapse">
+            <ul class="nav pull-right">
+              <li class="active"><a href="index.php">Home</a></li>
+              <li><a href="404.php">About Us</a></li>
+              <li class="dropdown"> <a data-toggle="dropdown" class="dropdown-toggle" href="#">Services<span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                  <li><a href="404.php">Home</a></li>
+                  <li><a href="404.php">Business</a></li>
+                </ul>
+              </li>
+              <li><a href="404.php">Blog</a></li>
+              <li><a href="contact.php">Contact</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <?php include("social_media_top.php"); ?>
+    </div>
+  </header>
+  <div class="full_page_photo">
+    <iframe width="100%" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=&amp;aq=&amp;sll=42.074272,-87.907791&amp;sspn=0.418446,1.012115&amp;t=h&amp;ie=UTF8&amp;ll=42.172564,-87.925644&amp;spn=0.417798,1.012115&amp;z=11&amp;output=embed"></iframe>
+  </div>
+  <div id="main">
+    <div class="container">
+      <section id="contact">
+        <div class="hgroup">
+          <h1>Contact us</h1>
+          <h2>This is a subtitle used optionally for explaining more...</h2>
+          <ul class="breadcrumb pull-right">
+            <li><a href="index.html">Home</a> <span class="divider">/</span></li>
+            <li class="active">Contact</li>
+          </ul>
+        </div>
+        <div class="row">
+          <div class="span4 office_address">
+            <address>
+              <strong>Voska Electronics</strong><br>
+              Northwest Chicagoland Area <br>
+              <abbr title="Phone">P:</abbr> (224) 633-9427
+            </address>
+            <address>
+              <strong>Matt Voska</strong><br>
+              <a href="mailto:#">matt@voskaelectronics.com</a>
+            </address>
+          </div>
+          <div class="span8 contact_form">
+            <form>
+              <div class="row">
+                <div class="span4">
+                  <label>Name</label>
+                  <input type="text" class="span4">
+                </div>
+                <div class="span4">
+                  <label>E-mail</label>
+                  <input type="text" class="span4">
+                </div>
+                <div class="span8">
+                  <label>Subject</label>
+                  <input type="text" class="span8">
+                </div>
+                <div class="span8">
+                  <label>Message</label>
+                  <textarea rows="8" class="span8"></textarea>
+                </div>
+                <div class="span8"> <a class="btn btn-large btn-success"><span>Submit Message</span></a></div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </section>
+      <!--END Contact-->
+    </div>
+    <?php include("footer.php"); ?>
+  </div>
+  <?php include("scripts.php"); ?>
+</body>
+</html>
